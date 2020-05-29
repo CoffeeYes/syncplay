28355b4cb31cd4caf8fa2aa930f7462dd282970b
CoffeeYes <culinecro@gmail.com>
2020-05-29 10:00:50 +0200

    text truncation;image sizing;content-spacing for videplaylist items

--- src/Playlist.js
+++ src/Playlist.js
@@ -9,8 +9,8 @@ class Playlist extends Component {
                         return (
                             <div className="playlistVideoContainer">
                                 <div className="playlistVideo" key={index} onClick={() => this.props.videoFromPlaylistWasClicked(item.videoID,index)}>
-                                    <img src={item.imgURL}/>
-                                    <p>{item.title}</p>
+                                    <img className="playlistVideoImg" src={item.imgURL}/>
+                                    <p className="playlistVideoTitle line-clamp-3">{item.title}</p>
                                 </div>
                                 <button className="removeFromPlaylistButton" onClick={() => this.props.removeVideoFromPlaylist(index)}>X</button>
                             </div>
