32a01f8c047232a4a61e336be9befa929aa1cd2b
CoffeeYes <culinecro@gmail.com>
2020-05-29 20:53:55 +0200

    fixed searchresults display

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -5,12 +5,14 @@ class SearchBar extends Component {
     render = () => {
         return (
             <div className="searchContainer">
-                <input id='searchBar' name='searchTerm' value={this.props.searchTerm} onChange={this.props.handleChange} onKeyPress={this.props.searchInputEnterPressed}/>
-                <SearchResult 
-                searchResults={this.props.searchResults} 
-                userClickedSearchResult={(videoID) => this.props.userClickedSearchResult(videoID)}
-                addVideoToPlaylist={(videoObj => this.props.addVideoToPlaylist(videoObj))}
-                />
+                <div className="searchBarAndResultsContainer">
+                    <input id='searchBar' name='searchTerm' value={this.props.searchTerm} onChange={this.props.handleChange} onKeyPress={this.props.searchInputEnterPressed}/>
+                    <SearchResult 
+                    searchResults={this.props.searchResults} 
+                    userClickedSearchResult={(videoID) => this.props.userClickedSearchResult(videoID)}
+                    addVideoToPlaylist={(videoObj => this.props.addVideoToPlaylist(videoObj))}
+                    />
+                </div>
             </div>
         )
     }
