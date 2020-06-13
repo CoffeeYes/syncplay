441f32052807b16d2c89d13cc2da8573a3fe16c7
CoffeeYes <culinecro@gmail.com>
2020-06-13 12:41:39 +0200

    changed splash page css

--- src/Splash.js
+++ src/Splash.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Splash extends Component {
     render = () => {
         return (
-            <div className="verticalCenter horizontalCenter">
+            <div className="verticalCenter horizontalCenter splash">
                 <div className="splashContentContainer">
                     <h1>Create a room, Send the link to your friends and watch together!</h1>
                     <button id="createRoomButton" onClick={this.props.createNewRoom}>Create Room</button>
