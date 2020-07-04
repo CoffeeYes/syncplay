978cebe434b7bfa8b182c4e878947ba9b6ae6696
CoffeeYes <culinecro@gmail.com>
2020-07-04 14:46:38 +0200

    cache accept restyling

--- src/CacheAccept.js
+++ src/CacheAccept.js
@@ -5,8 +5,10 @@ function CacheAccept (props) {
         <div className="cacheAcceptModal">
             <div className="cacheAcceptContent">
                 <p className="cacheAcceptText">This Website uses cookies.<br/>Click accept if you consent to the use of functional cookies.</p>
-                <button onClick={() => props.handleCacheChoice("reject")}>Reject</button>
-                <button onClick={() => props.handleCacheChoice("accept")}>Accept</button>
+                <div className="cacheButtonContainer">
+                    <button onClick={() => props.handleCacheChoice("reject")} className="cacheRejectButton">Reject</button>
+                    <button onClick={() => props.handleCacheChoice("accept")} className="defaultButton">Accept</button>
+                </div>
             </div>
         </div>
     )
