ec190ee2495bcb30d082575c1990371485cb9140
CoffeeYes <culinecro@gmail.com>
2020-07-17 11:44:29 +0200

    allow user to press enter to confirm name change

--- src/UsernameModal.js
+++ src/UsernameModal.js
@@ -6,12 +6,18 @@ function UsernameModal (props) {
             <div className="usernameModalChoice">
                 <p className="error">{props.nameError}</p>
                 <p>Choose your Username</p>
-                <input className="usernameChoiceInput inputFocus" name="changeName" value={props.changeName} onChange={props.handleChange}/>
+                <input
+                className="usernameChoiceInput inputFocus"
+                name="changeName"
+                value={props.changeName}
+                onChange={props.handleChange}
+                onKeyPress={(event) => {return event.which == 13 ? props.changeUsername() : false}}
+                />
                 <button className="defaultButton changeNameButton" onClick={props.changeUsername}>Submit</button>
             </div>
         </div>
     )
-    
+
 }
 
 export default UsernameModal
