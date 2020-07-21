081ff2fa0f283b1bb41761a456e7c5e56993005b
CoffeeYes <culinecro@gmail.com>
2020-07-21 11:56:18 +0200

    very basic portrait mobile layout

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -10,17 +10,20 @@ class SearchBar extends Component {
                 <a href="/">
                     <img src={Logo} id="logoImg"/>
                 </a>
-                <div className="copyLinkContainer">
+                <div className="copyLinkContainer copyLinkButton">
                     {this.props.linkCopied && <p>Copied!</p>}
                     <button className="defaultButton" onClick={this.props.copyLink}>Copy Link</button>
                 </div>
                 <div className="searchBarAndResultsContainer">
                     <p className="error">{this.props.error}</p>
-                    <input className='searchBar inputFocus' name='searchTerm' value={this.props.searchTerm} onChange={this.props.handleChange} onKeyPress={this.props.searchInputEnterPressed}
+                    <input className='searchBar inputFocus'
+                    name='searchTerm' value={this.props.searchTerm}
+                    onChange={this.props.handleChange}
+                    onKeyPress={this.props.searchInputEnterPressed}
                     placeholder="Search or paste a youtube url here ..."
                     />
-                    <SearchResult 
-                    searchResults={this.props.searchResults} 
+                    <SearchResult
+                    searchResults={this.props.searchResults}
                     userClickedSearchResult={(videoID) => this.props.userClickedSearchResult(videoID)}
                     addVideoToPlaylist={(videoObj => this.props.addVideoToPlaylist(videoObj))}
                     />
