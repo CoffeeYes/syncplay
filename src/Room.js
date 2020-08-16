7f1548df09c23de17427c6b884d1292be88f11ef
CoffeeYes <culinecro@gmail.com>
2020-08-16 12:54:35 +0200

    rudimentary rewrite of room component to functional component using hooks

--- src/Room.js
+++ src/Room.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Player from './Player.js'
 import SearchBar from './SearchBar.js'
 
@@ -7,68 +7,68 @@ import Playlist from './Playlist'
 import BugReport from './BugReport'
 import UsernameModal from './UsernameModal'
 import CacheAccept from './CacheAccept.js';
-class Room extends Component {
 
-    componentDidMount = () => {
+const Room = props => {
+
+    useEffect(() => {
         //re-hydrate room state after losing on redirect
         let roomID = window.location.href.split("/room/")[1]
-        this.props.initializeRoom(roomID)
-    }
-    render = () => {
-        return(
-            <div className="mainContainerRoom">
-                {this.props.showCacheDialogue &&
-                <CacheAccept
-                handleCacheChoice={(choice) => this.props.handleCacheChoice(choice)}
+        props.initializeRoom(roomID)
+    },[])
+
+    return(
+        <div className="mainContainerRoom">
+            {props.showCacheDialogue &&
+            <CacheAccept
+            handleCacheChoice={(choice) => props.handleCacheChoice(choice)}
+            />
+            }
+            { props.showUsernameModal &&
+                <UsernameModal
+                changeName={props.changeName}
+                handleChange={props.handleChange}
+                changeUsername={props.changeUsername}
+                nameError={props.nameError}
                 />
-                }
-                { this.props.showUsernameModal &&
-                    <UsernameModal
-                    changeName={this.props.changeName}
-                    handleChange={this.props.handleChange}
-                    changeUsername={this.props.changeUsername}
-                    nameError={this.props.nameError}
-                    />
-                }
-                <SearchBar
-                handleChange={this.props.handleChange}
-                searchInputEnterPressed={this.props.searchInputEnterPressed}
-                searchResults={this.props.searchResults}
-                searchTerm={this.props.searchTerm}
-                userClickedSearchResult={(videoID) => this.props.userClickedSearchResult(videoID)}
-                addVideoToPlaylist={(videoObj => this.props.addVideoToPlaylist(videoObj))}
-                error={this.props.error}
-                triggerBugReport={this.props.triggerBugReport}
-                copyLink={this.props.copyLink}
-                linkCopied={this.props.linkCopied}
-                showAddToPlaylistFromURLButton={this.props.showAddToPlaylistFromURLButton}
-                addToPlaylistFromURL={this.props.addToPlaylistFromURL}
-                toggleBlockMinimize={this.props.toggleBlockMinimize}
-                blockMinimize={this.props.blockMinimize}
-                toggleAutoPlay={this.props.toggleAutoPlay}
-                autoPlay={this.props.autoPlay}
-                hideSearchOnExit={event => this.props.hideSearchOnExit(event)}
-                />
-                <Player videoSource={this.props.videoSource}/>
-                <Playlist
-                playlistVideos={this.props.playlistVideos}
-                videoFromPlaylistWasClicked={(videoID, index) => this.props.videoFromPlaylistWasClicked(videoID, index)}
-                removeVideoFromPlaylist={(index) => this.props.removeVideoFromPlaylist(index)}
-                />
-                <ChatBox
-                localMessage={this.props.localMessage}
-                messages={this.props.messages}
-                sendMessage={this.props.sendMessage}
-                handleChange={this.props.handleChange}
-                changeName={this.props.changeName}
-                changeUsername={this.props.changeUsername}
-                chatError={this.props.chatError}
-                connectedUsers={this.props.connectedUsers}
-                />
-                {this.props.showBugReport && <BugReport closeBugReport={this.props.closeBugReport} submitBugReport={this.props.submitBugReport}/>}
-            </div>
-        )
-    }
+            }
+            <SearchBar
+            handleChange={props.handleChange}
+            searchInputEnterPressed={props.searchInputEnterPressed}
+            searchResults={props.searchResults}
+            searchTerm={props.searchTerm}
+            userClickedSearchResult={(videoID) => props.userClickedSearchResult(videoID)}
+            addVideoToPlaylist={(videoObj => props.addVideoToPlaylist(videoObj))}
+            error={props.error}
+            triggerBugReport={props.triggerBugReport}
+            copyLink={props.copyLink}
+            linkCopied={props.linkCopied}
+            showAddToPlaylistFromURLButton={props.showAddToPlaylistFromURLButton}
+            addToPlaylistFromURL={props.addToPlaylistFromURL}
+            toggleBlockMinimize={props.toggleBlockMinimize}
+            blockMinimize={props.blockMinimize}
+            toggleAutoPlay={props.toggleAutoPlay}
+            autoPlay={props.autoPlay}
+            hideSearchOnExit={event => props.hideSearchOnExit(event)}
+            />
+            <Player videoSource={props.videoSource}/>
+            <Playlist
+            playlistVideos={props.playlistVideos}
+            videoFromPlaylistWasClicked={(videoID, index) => props.videoFromPlaylistWasClicked(videoID, index)}
+            removeVideoFromPlaylist={(index) => props.removeVideoFromPlaylist(index)}
+            />
+            <ChatBox
+            localMessage={props.localMessage}
+            messages={props.messages}
+            sendMessage={props.sendMessage}
+            handleChange={props.handleChange}
+            changeName={props.changeName}
+            changeUsername={props.changeUsername}
+            chatError={props.chatError}
+            connectedUsers={props.connectedUsers}
+            />
+            {props.showBugReport && <BugReport closeBugReport={props.closeBugReport} submitBugReport={props.submitBugReport}/>}
+        </div>
+    )
 }
 
 export default Room
