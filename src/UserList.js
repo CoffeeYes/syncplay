d127e1d410f34a1da59d0407e5c7e803b3090d43
CoffeeYes <culinecro@gmail.com>
2020-08-25 18:22:50 +0200

    rewrote userList as functional component

--- src/UserList.js
+++ src/UserList.js
@@ -1,17 +1,22 @@
-import React, {Component} from 'react'
+import React, { useState, useEffect } from 'react'
+import socket from './Socket'
 
-class UserList extends Component {
-    render = () => {
-        return (
-            <div className="userListContainer">
-                <p className="connectedUserText">Connected Users :</p>
-                {this.props.connectedUsers.map((item,index) => {
-                    return <p key={index} className="connectedUserText">{item},</p>
-                })}
-            </div>
-        )
-    }
-    
+const UserList = props => {
+    const [connectedUsers,setConnectedUsers] = useState([])
+
+    useEffect(() => {
+        socket.on("receiveConnectedUsers",users => {
+            setConnectedUsers(users)
+        })
+    },[])
+
+    return (
+        <div className="userListContainer">
+        <p className="connectedUserText">Connected Users :</p>
+        {connectedUsers.map((item,index) => {
+            return <p key={index} className="connectedUserText">{item},</p>
+        })}
+        </div>
+    )
 }
-
 export default UserList
--- src/UsernameModal.js
+++ src/UsernameModal.js
@@ -1,23 +1,19 @@
 import React from 'react'
 
-function UsernameModal (props) {
-    return(
-        <div className="usernameModalFullscreen">
-            <div className="usernameModalChoice">
-                <p className="error">{props.nameError}</p>
-                <p>Choose your Username</p>
-                <input
-                className="usernameChoiceInput inputFocus"
-                name="changeName"
-                value={props.changeName}
-                onChange={props.handleChange}
-                onKeyPress={(event) => {return event.which == 13 ? props.changeUsername() : false}}
-                />
-                <button className="defaultButton changeNameButton" onClick={props.changeUsername}>Submit</button>
-            </div>
+const UsernameModal = props =>
+    <div className="usernameModalFullscreen">
+        <div className="usernameModalChoice">
+            <p className="error">{props.nameError}</p>
+            <p>Choose your Username</p>
+            <input
+            className="usernameChoiceInput inputFocus"
+            name="changeName"
+            value={props.changeName}
+            onChange={props.handleChange}
+            onKeyPress={(event) => {return event.which == 13 ? props.changeUsername() : false}}
+            />
+            <button className="defaultButton changeNameButton" onClick={props.changeUsername}>Submit</button>
         </div>
-    )
-
-}
+    </div>
 
 export default UsernameModal
