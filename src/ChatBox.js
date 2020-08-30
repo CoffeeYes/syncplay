296aaba21e6614cad04103bb994eda650d861d64
CoffeeYes <culinecro@gmail.com>
2020-08-30 12:15:26 +0200

    rewrote chatbox as functional component, removed redundant code

--- src/ChatBox.js
+++ src/ChatBox.js
@@ -1,62 +1,56 @@
-import React, {Component} from 'react'
+import React, {Component, useState, useEffect} from 'react'
 import UserList from './UserList.js'
 
-class ChatBox extends Component {
-    constructor(props) {
-        super(props)
+const ChatBox = props => {
+    const msgEndRef = React.createRef();
 
-        this.msgEndRef = React.createRef();
+    const scrollToBottom = () => {
+        msgEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
 
-    componentDidUpdate = () => {
-        this.scrollToBottom();
-    }
+    useEffect(() => {
+      scrollToBottom();
+    },[props.messages])
 
-    scrollToBottom = () => {
-        this.msgEndRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-
-    render = () => {
-        return(
-            <div className="chatContainer">
-                <p className="error">{this.props.chatError}</p>
-                <UserList connectedUsers={this.props.connectedUsers}/>
-                <div className="messageBox" ref={this.msgBoxRef}>
-                    {this.props.messages.map((item,index) => {
-                        if(item.sentFromHere) {
-                          return(
-                              <div className="message messageFromHere" style={{"backgroundColor" : item.color}} key={index}>
-                                  <div className="messageContent">
-                                      <div className="messageHeader">
-                                          <p className="messageUser"><b>{item.user}</b></p>
-                                          <p className="messageTime"><b>{item.time}</b></p>
-                                      </div>
-                                      <p className="messageText">{item.text}</p>
+    return(
+        <div className="chatContainer">
+            <p className="error">{props.chatError}</p>
+            <UserList connectedUsers={props.connectedUsers}/>
+            <div className="messageBox">
+                {props.messages.map((item,index) => {
+                    if(item.sentFromHere) {
+                      return(
+                          <div className="message messageFromHere" style={{"backgroundColor" : item.color}} key={index}>
+                              <div className="messageContent">
+                                  <div className="messageHeader">
+                                      <p className="messageUser"><b>{item.user}</b></p>
+                                      <p className="messageTime"><b>{item.time}</b></p>
                                   </div>
+                                  <p className="messageText">{item.text}</p>
                               </div>
-                          )
-                        }
-                        else {
-                          return(
-                              <div className="message messageFromOther" style={{"backgroundColor" : item.color}} key={index}>
-                                  <div className="messageContent">
-                                      <div className="messageHeader">
-                                          <p className="messageUser"><b>{item.user}</b></p>
-                                          <p className="messageTime"><b>{item.time}</b></p>
-                                      </div>
-                                      <p className="messageText">{item.text}</p>
+                          </div>
+                      )
+                    }
+                    else {
+                      return(
+                          <div className="message messageFromOther" style={{"backgroundColor" : item.color}} key={index}>
+                              <div className="messageContent">
+                                  <div className="messageHeader">
+                                      <p className="messageUser"><b>{item.user}</b></p>
+                                      <p className="messageTime"><b>{item.time}</b></p>
                                   </div>
+                                  <p className="messageText">{item.text}</p>
                               </div>
-                          )
-                        }
-                    })}
-                    <div ref={this.msgEndRef}/>
-                </div>
-                <input id="chatInput" name="localMessage" onChange={this.props.handleChange} onKeyPress={this.props.sendMessage} value={this.props.localMessage}
-                placeholder="Send a message to the room"/>
+                          </div>
+                      )
+                    }
+                })}
+                <div ref={msgEndRef}/>
             </div>
-        )
-    }
+            <input id="chatInput" name="localMessage" onChange={props.handleChange} onKeyPress={props.sendMessage} value={props.localMessage}
+            placeholder="Send a message to the room"/>
+        </div>
+    )
 }
 
 
