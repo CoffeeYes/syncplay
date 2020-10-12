8eba2cc836596b348d19aac649a027ea7fb77274
CoffeeYes <culinecro@gmail.com>
2020-10-12 18:07:24 +0200

    added template theme

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
 
+const theme = createMuiTheme({
+
+});
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
     </BrowserRouter>,
   document.getElementById('root')
 );
